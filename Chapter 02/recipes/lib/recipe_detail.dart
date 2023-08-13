523b60878341830d02419294ac90dc1f0858e9a4
DittoGod <simba.ndoro@dittogod.com>
2023-08-13 17:14:17 +1000

    Added slider to increase ingredients by number of servings.

--- Chapter 02/recipes/lib/recipe_detail.dart
+++ Chapter 02/recipes/lib/recipe_detail.dart
@@ -15,7 +15,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
   @override
   Widget build(BuildContext context) {
     // Scaffold defines the page’s general structure.
@@ -45,8 +45,42 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
-            // TODO: Add Expanded
-            // TODO: Add Slider() here
+            // An Expanded widget, which expands to fill the space in a Column. This way, the ingredient list will take up the space not filled by the other widgets.
+            Expanded(
+              // A ListView, with one row per ingredient.
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // A Text that uses string interpolation to populate a string with runtime values. You use the ${expression} syntax inside the string literal to denote these.
+                  return Text(
+                    '${ingredient.quantity * _sliderVal} '
+                    '${ingredient.measure} '
+                    '${ingredient.name}',
+                  );
+                },
+              ),
+            ),
+            Slider(
+              // You use min, max and divisions to define how the slider moves. In this case, it moves between the values of 1 and 10, with ten discreet stops. That is, it can only have values of 1, 2, 3, 4, 5, 6, 7, 8, 9 or 10.
+              min: 1,
+              max: 10,
+              divisions: 9,
+              // label updates as the _sliderVal changes and displays a scaled number of servings.
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              // The slider works in double values, so this converts the int variable.
+              value: _sliderVal.toDouble(),
+              // Conversely, when the slider changes, this uses round() to convert the double slider value to an int, then saves it in _sliderVal.
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              // This sets the slider’s colors to something more “on brand”. The activeColor is the section between the minimum value and the thumb, and the inactiveColor represents the rest.
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
