83ec56aee7cfddec3bb15b96b7c0fc077efbd9e5
DittoGod <simba.ndoro@dittogod.com>
2023-08-18 13:58:23 +1000

    Created main.

--- Chapter 03/projects/starter/lib/main.dart
+++ Chapter 03/projects/starter/lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // 1
+  // Everything in Flutter starts with a widget. runApp() takes in the root
+  // widget Fooderlich.
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
+  // Every stateless widget must override the build() method.
   const Fooderlich({super.key});
   @override
   Widget build(BuildContext context) {
     // TODO: Create theme
     // TODO: Apply Home widget
-    // 3
+    // The Fooderlich widget starts by composing a MaterialApp widget to give it a Material Design system look and feel. See https://material.io for more details about it.
     return MaterialApp(
       // TODO: Add theme
       title: 'Fooderlich',
-      // 4
+      // The MaterialApp widget contains a Scaffold widget, which defines the
+      // layout and structure of the app. The scaffold has two properties: an
+      // appBar and a body. An Appbar’s title contains a Text widget. The body
+      // has a Center widget, whose child property is a Text widget.
       home: Scaffold(
         // TODO: Style the title
         appBar: AppBar(title: const Text('Fooderlich')),
