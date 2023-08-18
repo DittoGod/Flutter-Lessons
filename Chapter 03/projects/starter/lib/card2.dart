bcf601bb21c5e2cbbeecc2e228620cbced9ad783
DittoGod <simba.ndoro@dittogod.com>
2023-08-18 22:20:41 +1000

    Started creating second card.

---
+++ Chapter 03/projects/starter/lib/card2.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // The Center widget has a Container child widget which has three
+      // properties, the first two being constraints and decoration.
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag5.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        // The third property is child and it has a Column widget, which
+        // displays its children vertically.
+        child: Column(
+          children: [
+            // TODO: Add author information
+            // TODO: Add Positioned text
+          ],
+        ),
+      ),
+    );
+  }
+}
