f6247e47de1f23a99c188604b342ea3255ddb6ef
DittoGod <simba.ndoro@dittogod.com>
2023-08-18 15:46:21 +1000

    Linked main with home.

--- Chapter 03/projects/starter/lib/main.dart
+++ Chapter 03/projects/starter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // Imported the FooderlichTheme.
 import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
   // Everything in Flutter starts with a widget. runApp() takes in the root
@@ -18,29 +19,9 @@ class Fooderlich extends StatelessWidget {
     // TODO: Apply Home widget
     // The Fooderlich widget starts by composing a MaterialApp widget to give it a Material Design system look and feel. See https://material.io for more details about it.
     return MaterialApp(
-      // Added the MaterialApp widget’s theme property.
       theme: theme,
       title: 'Fooderlich',
-      // The MaterialApp widget contains a Scaffold widget, which defines the
-      // layout and structure of the app. The scaffold has two properties: an
-      // appBar and a body. An Appbar’s title contains a Text widget. The body
-      // has a Center widget, whose child property is a Text widget.
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            // Added AppBar text styling.
-            style: theme.textTheme.titleLarge,
-          ),
-        ),
-        body: Center(
-          child: Text(
-            'Let\'s get cooking ! ',
-            // Finally, added body text styling.
-            style: theme.textTheme.displayLarge,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
