2588708e465c0cc9c2cfe00c7b7f4168ff95a864
DittoGod <simba.ndoro@dittogod.com>
2023-08-18 14:08:42 +1000

    Linked to theme file.

--- Chapter 03/projects/starter/lib/main.dart
+++ Chapter 03/projects/starter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
   // Everything in Flutter starts with a widget. runApp() takes in the root
@@ -11,21 +12,29 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({super.key});
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     // The Fooderlich widget starts by composing a MaterialApp widget to give it a Material Design system look and feel. See https://material.io for more details about it.
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
       // The MaterialApp widget contains a Scaffold widget, which defines the
       // layout and structure of the app. The scaffold has two properties: an
       // appBar and a body. An Appbar’s title contains a Text widget. The body
       // has a Center widget, whose child property is a Text widget.
       home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.titleLarge,
+          ),
+        ),
+        body: Center(
+          child: Text(
+            'Let\'s get cooking ! ',
+            style: theme.textTheme.displayLarge,
+          ),
+        ),
       ),
     );
   }
