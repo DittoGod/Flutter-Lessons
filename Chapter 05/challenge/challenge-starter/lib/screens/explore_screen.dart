aead0cb7d53d4f02f46631fea6b735a2899417c8
DittoGod <simba.ndoro@dittogod.com>
2023-09-14 15:37:47 +1000

    Finished challenge.

--- Chapter 05/challenge/challenge-starter/lib/screens/explore_screen.dart
+++ Chapter 05/challenge/challenge-starter/lib/screens/explore_screen.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as developer;
 
 import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
-  final mockService = MockFooderlichService();
-
+class ExploreScreen extends StatefulWidget {
   ExploreScreen({super.key});
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _scrollController;
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+        _scrollController.position.maxScrollExtent) {
+      developer.log('i am at the bottom!');
+    }
+    if (_scrollController.offset <=
+        _scrollController.position.minScrollExtent) {
+      developer.log('i am at the top!');
+    }
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +42,7 @@ class ExploreScreen extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
             scrollDirection: Axis.vertical,
+            controller: _scrollController,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
               const SizedBox(height: 16),
