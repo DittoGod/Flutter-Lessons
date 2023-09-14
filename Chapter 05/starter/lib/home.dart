c5684e54112d320ead39ddfe23161ad8f39171aa
DittoGod <simba.ndoro@dittogod.com>
2023-09-14 14:23:57 +1000

    Added recipe screen to home.

--- Chapter 05/starter/lib/home.dart
+++ Chapter 05/starter/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,8 +14,7 @@ class HomeState extends State<Home> {
 
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
-    // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
+    RecipesScreen(),
     Container(color: Colors.blue),
   ];
 
