ecc2f4c112a0855ce4fc174b2ac5277931afa79a
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 19:48:59 +1000

    Created grocery list view.

--- Chapter 06/starter/lib/screens/grocery_list_screen.dart
+++ Chapter 06/starter/lib/screens/grocery_list_screen.dart
@@ -13,7 +13,39 @@ class GroceryListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Replace with ListView
-    return Container();
+    // Get the list of grocery items from the manager.
+    final groceryItems = manager.groceryItems;
+    // Apply padding of 16 pixels all around this screen.
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      // Add ListView.
+      child: ListView.separated(
+        // Set the number of items in the list.
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          final item = groceryItems[index];
+          // TODO: Wrap in a Dismissable
+          // TODO: Wrap in an InkWell
+          // For each item in the list, get the current item and construct a
+          // GroceryTile.
+          return GroceryTile(
+            key: Key(item.id),
+            item: item,
+            // Return onComplete when the user taps the checkbox.
+            onComplete: (change) {
+              // Check if there is a change and update the item’s isComplete
+              // status.
+              if (change != null) {
+                manager.completeItem(index, change);
+              }
+            },
+          );
+        },
+        // Space each grocery item 16 pixels apart.
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16.0);
+        },
+      ),
+    );
   }
 }
