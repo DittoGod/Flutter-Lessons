39c49469146f5f9d77cae7b420e75559fea2a86f
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 16:22:17 +1000

    Added navigation to grocery item screen in grocery screen.

--- Chapter 06/starter/lib/screens/grocery_screen.dart
+++ Chapter 06/starter/lib/screens/grocery_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
@@ -17,7 +18,33 @@ class GroceryScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          // TODO: Present GroceryItemScreen
+          // Returns the GroceryManager available in the tree.
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          // Navigator.push() adds a new route to the stack of routes.
+          Navigator.push(
+            context,
+            // MaterialPageRoute replaces the entire screen with a
+            // platform-specific transition. In Android, for example, it slides
+            // upwards and fades in. In iOS, it slides in from the right.
+            MaterialPageRoute(
+              // Creates a new GroceryItemScreen within the route’s builder
+              // callback.
+              builder: (context) => GroceryItemScreen(
+                // onCreate defines what to do with the created item.
+                onCreate: (item) {
+                  // addItem() adds this new item to the list of items.
+                  manager.addItem(item);
+                  // Once the item is added to the list, pop() removes the top
+                  // navigation route item, GroceryItemScreen, to show the list
+                  // of grocery items.
+                  Navigator.pop(context);
+                },
+                // onUpdate will never get called since you are creating a new
+                // item.
+                onUpdate: (item) {},
+              ),
+            ),
+          );
         },
       ),
       // Builds the rest of the Grocery screen’s subtree. That’s coming up next!
