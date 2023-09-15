b6f6a718157b02d7ecc369f5fb795b86ca83cf5b
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 13:53:36 +1000

    Updated layout and added grocery consumer to grocery screen.

--- Chapter 06/starter/lib/screens/grocery_screen.dart
+++ Chapter 06/starter/lib/screens/grocery_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/models.dart';
 import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -7,8 +9,39 @@ class GroceryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    // The main layout structure for GroceryScreen is a scaffold.
+    return Scaffold(
+      // Adds a floating action button with a + icon. Tapping the button
+      // presents the screen to create or add an item. You’ll build this
+      // screen later.
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO: Present GroceryItemScreen
+        },
+      ),
+      // Builds the rest of the Grocery screen’s subtree. That’s coming up next!
+      body: buildGroceryScreen(),
+    );
   }
-  // TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    // You wrap your widgets inside a Consumer, which listens for GroceryManager
+    // state changes.
+    return Consumer<GroceryManager>(
+      // Consumer rebuilds the widgets below itself when the grocery manager
+      // items changes.
+      builder: (context, manager, child) {
+        // If there are grocery items in the list, show the GroceryListScreen.
+        // You will create this screen soon.
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO: Add GroceryListScreen
+          return Container();
+        } else {
+          // If there are no grocery items, show the EmptyGroceryScreen.
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
