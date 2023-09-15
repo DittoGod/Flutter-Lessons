0e70a64da5b347de80d94729cf85a9fa342aa1d4
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 11:25:26 +1000

    Started chapter 06.

---
+++ Chapter 06/final/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          ChangeNotifierProvider(create: (context) => GroceryManager()),
+        ],
+        child: const Home(),
+      ),
+    );
+  }
+}
