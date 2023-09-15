01523c168493d974616b139d061bef1d757f6114
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 13:03:52 +1000

    Added layout widgets to empty grocery screen.

--- Chapter 06/starter/lib/screens/empty_grocery_screen.dart
+++ Chapter 06/starter/lib/screens/empty_grocery_screen.dart
@@ -5,7 +5,22 @@ class EmptyGroceryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    // Padding: Adds 30 pixels on all sides.
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      // Center: Places all the other widgets in the center.
+      child: Center(
+        // Column: Handles the vertical layouts of the other widgets.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // TODO: Add empty image
+            // TODO: Add empty screen title
+            // TODO: Add empty screen subtitle
+            // TODO: Add browse recipes button
+          ],
+        ),
+      ),
+    );
   }
 }
