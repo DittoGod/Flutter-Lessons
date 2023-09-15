11739a8f68ddb760d536d80b1fec5752a9c3a800
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 12:54:57 +1000

    Started creating grocery screen.

---
+++ Chapter 06/starter/lib/screens/grocery_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace with EmptyGroceryScreen
+    return Container(color: Colors.green);
+  }
+  // TODO: Add buildGroceryScreen
+}
