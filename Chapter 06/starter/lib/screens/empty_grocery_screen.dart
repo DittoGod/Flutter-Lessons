b8fc83ed91dd57188fad8609e95d77d7dde9cb4f
DittoGod <simba.ndoro@dittogod.com>
2023-09-15 12:58:55 +1000

    Started creating the empty grocery screen.

---
+++ Chapter 06/starter/lib/screens/empty_grocery_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EmptyGroceryScreen extends StatelessWidget {
+  const EmptyGroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace and add layout widgets
+    return Container(color: Colors.purple);
+  }
+}
