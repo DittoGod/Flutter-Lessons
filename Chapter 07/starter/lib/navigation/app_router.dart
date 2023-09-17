9479665b5fdd21e6bd3820a8fd8e647fda7c67ae
DittoGod <simba.ndoro@dittogod.com>
2023-09-17 11:35:51 +1000

    Started creating the app router.

---
+++ Chapter 07/starter/lib/navigation/app_router.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+class AppRouter {
+  // Declares AppStateManager. GoRouter will listen to app state changes to
+  // handle errors or redirects.
+  final AppStateManager appStateManager;
+  // Declares ProfileManager to get the user’s profile.
+  final ProfileManager profileManager;
+  // Declares GroceryManager to manage when the user creates or edits a grocery
+  // item.
+  final GroceryManager groceryManager;
+
+  AppRouter(
+    this.appStateManager,
+    this.profileManager,
+    this.groceryManager,
+  );
+
+  // Creates a variable that holds a GoRouter instance.
+  late final router = GoRouter(
+    // Enables debugging. This is especially useful to see what path your user
+    // is going to and detect any problems with your routes.
+    // TODO: remove the debugLogDiagnostics flag before you ship your app
+    debugLogDiagnostics: true,
+    // Sets your router to listen for app state changes. When the state
+    // changes, your router will trigger a rebuild of your routes.
+    refreshListenable: appStateManager,
+    // Sets the initial location for routing.
+    initialLocation: '/login',
+    // Defines all the routes you use. You’ll add them later.
+    routes: [
+      // TODO: Add Login Route
+      // TODO: Add Onboarding Route
+      // TODO: Add Home Route
+    ],
+    // TODO: Add Error Handler
+    // TODO: Add Redirect Handler
+  );
+}
