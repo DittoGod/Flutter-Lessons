df4ed14509ee2d9b3bd11cdc00924048e22b403e
DittoGod <simba.ndoro@dittogod.com>
2023-09-17 11:41:53 +1000

    Added error handler to app router.

--- Chapter 07/starter/lib/navigation/app_router.dart
+++ Chapter 07/starter/lib/navigation/app_router.dart
@@ -36,7 +36,20 @@ class AppRouter {
       // TODO: Add Onboarding Route
       // TODO: Add Home Route
     ],
-    // TODO: Add Error Handler
+
+    errorPageBuilder: (context, state) {
+      return MaterialPage(
+        key: state.pageKey,
+        child: Scaffold(
+          body: Center(
+            child: Text(
+              state.error.toString(),
+            ),
+          ),
+        ),
+      );
+    },
+
     // TODO: Add Redirect Handler
   );
 }
