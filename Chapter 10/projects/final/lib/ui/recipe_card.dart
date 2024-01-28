12d8ef83d5fc2da5da44d97e4d742005d69f1764
DittoGod <simba.ndoro@dittogod.com>
2024-01-28 18:15:55 +0000

    Started Chapter 10

---
+++ Chapter 10/projects/final/lib/ui/recipe_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+Widget recipeStringCard(String image, String label) {
+  return Card(
+    elevation: 4.0,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(6.0),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(6.0), topRight: Radius.circular(6.0)),
+          child: Image.asset(
+            'assets/images/pizza_w700.png',
+            height: 200,
+            width: 200,
+          ),
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            label,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Text(
+            '320CAL',
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 11,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+      ],
+    ),
+  );
+}
