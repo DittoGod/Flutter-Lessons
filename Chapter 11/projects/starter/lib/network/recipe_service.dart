f385fd5936b79cf68c5f18617a22e24d2cc8059b
DittoGod <simba.ndoro@dittogod.com>
2024-02-10 23:01:57 +0000

    Switched to windows.

---
+++ Chapter 11/projects/starter/lib/network/recipe_service.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+import 'package:http/http.dart';
+import 'env.dart';
+
+String apiKey = Env.APIKEY.toString();
+String apiId = Env.APPID.toString();
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  // getData() returns a Future (with an upper case “F”) because an API’s
+  // returned data type is determined in the future (lower case “f”). async
+  // signifies this method is an asynchronous operation.
+  Future getData(String url) async {
+    log('$apiKey \n$apiId');
+    // response doesn’t have a value until await completes. Response and get()
+    // are from the HTTP package. get() fetches data from the provided url.
+    final response = await get(Uri.parse(url));
+    // A statusCode of 200 means the request was successful.
+    if (response.statusCode == 200) {
+      // You return the results embedded in response.body.
+      return response.body;
+    } else {
+      // Otherwise, you have an error — print the statusCode to the console.
+      log(response.body);
+    }
+  }
+// TODO: Add getRecipes
+}
