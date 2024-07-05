2d8ef718124a17acfe40e76ce3734f8b630b5ae2
Slyro Dittogod <slyro@dittogod.com>
2024-07-05 20:14:16 +0100

    Updated recipe_service.

--- Chapter 12/projects/starter/lib/network/recipe_service.dart
+++ Chapter 12/projects/starter/lib/network/recipe_service.dart
@@ -1,25 +1,45 @@
-import 'dart:developer';
-
-import 'package:http/http.dart';
+// This adds the Chopper package and your models.
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
 import 'env.dart';
 
+// Here is where you re-enter your API Key and ID.
 const String apiKey = Env.apiKey;
 const String apiId = Env.appID;
-const String apiUrl = 'https://api.edamam.com/search';
+// The /search was removed from the URL so that you can call other APIs
+// besides /search.
+const String apiUrl = 'https://api.edamam.com';
 
-class RecipeService {
-  Future getData(String url) async {
-    final response = await get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      log(response.body);
-    }
-  }
+// @ChopperApi() tells the Chopper generator to build a part file. This
+// generated file will have the same name as this file, but with .chopper added
+// to it. In this case, it will be recipe_service.chopper.dart. Such a file will
+// hold the boilerplate code.
+@ChopperApi()
+// RecipeService is an abstract class because you only need to define the method
+// signatures. The generator script will take these definitions and generate all
+// the code needed.
+abstract class RecipeService extends ChopperService {
+  // @Get is an annotation that tells the generator this is a GET request with
+  // a path named search, which you previously removed from the apiUrl. There
+  // are other HTTP methods you can use, such as @Post, @Put and @Delete, but
+  // you won’t use them in this chapter.
+  @Get(path: 'search')
+  // You define a function that returns a Future of a Response using the
+  // previously created APIRecipeQuery. The abstract Result that you created
+  // above will hold either a value or an error.
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+    // queryRecipes() uses the Chopper @Query annotation to accept a query
+    // string and from and to integers. This method doesn’t have a body. The
+    // generator script will create the body of this function with all the
+    // parameters.
+    @Query('q') String query,
+    @Query('from') int from,
+    @Query('to') int to,
+  );
 
-  Future<dynamic> getRecipes(String query, int from, int to) async {
-    final recipeData = await getData(
-        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
-    return recipeData;
-  }
+  // TODO: Add create()
 }
+
+// TODO: Add _addQuery()
