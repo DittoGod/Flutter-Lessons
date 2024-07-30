0bb74413b57f4c93a346a350010e7280832955cb
Slyro Dittogod <slyro@dittogod.com>
2024-07-30 15:09:41 +0100

    Started creating post_card.

---
+++ Chapter 03/projects/starter/lib/components/post_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:yummy/models/post.dart';
+
+class PostCard extends StatelessWidget {
+  final Post post;
+
+  const PostCard({
+    super.key,
+    required this.post,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme.apply(
+          displayColor: Theme.of(context).colorScheme.onSurface,
+        );
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // TODO: Add CircleAvatar
+            // TODO: Add spacing
+            // TODO: Add Expanded Widget
+          ],
+        ),
+      ),
+    );
+  }
+}
