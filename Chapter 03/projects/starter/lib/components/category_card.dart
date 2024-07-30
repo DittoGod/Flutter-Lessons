70fecd51cfb925aa5eddde9b1947d2cab9a091a5
Slyro Dittogod <slyro@dittogod.com>
2024-07-30 14:05:29 +0100

    Started creating category card.

---
+++ Chapter 03/projects/starter/lib/components/category_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:yummy/models/food_category.dart';
+
+class CategoryCard extends StatelessWidget {
+  // It simply takes in a FoodCategory object, which you'll use later to display
+  // data in the UI.
+  final FoodCategory category;
+
+  const CategoryCard({
+    super.key,
+    required this.category,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Get text theme.
+
+    // TODO: Replace with Card widget
+    return Container(); // Returns empty container.
+  }
+}
