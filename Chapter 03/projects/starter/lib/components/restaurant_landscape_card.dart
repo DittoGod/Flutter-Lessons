4fe2e65e52ae797c985e649afddf2f7fffa56b85
Slyro Dittogod <slyro@dittogod.com>
2024-07-31 17:48:11 +0100

    Finished creating RestaurantLandscapeCard.

--- Chapter 03/projects/starter/lib/components/restaurant_landscape_card.dart
+++ Chapter 03/projects/starter/lib/components/restaurant_landscape_card.dart
@@ -11,7 +11,7 @@ class RestaurantLandscapeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme.apply(
+    final TextTheme textTheme = Theme.of(context).textTheme.apply(
           displayColor: Theme.of(context).colorScheme.onSurface,
         );
 
@@ -19,8 +19,28 @@ class RestaurantLandscapeCard extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // TODO: Add Image
-          // TODO: Add ListTile
+          ClipRRect(
+            // borderRadius rounds the top corners with an 8.0 unit radius.
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(8.0)),
+            // AspectRatio displays an image with a 2:1 width-to-height ratio.
+            // The image scales to fit its container.
+            child: AspectRatio(
+              aspectRatio: 2,
+              child: Image.asset(restaurant.imageUrl, fit: BoxFit.cover),
+            ),
+          ),
+          ListTile(
+            // title shows the restaurants name with a specific style.
+            title: Text(restaurant.name, style: textTheme.titleSmall),
+            // subtitle displays the restaurants attributes, truncated if more
+            // than one line.
+            subtitle: Text(restaurant.attributes, style: textTheme.bodySmall),
+            // onTap prints the restaurant's name to the console when tapped.
+            onTap: () {
+              // ignore: avoid_print
+              print('Tap on ${restaurant.name}');
+            },
+          ),
         ],
       ),
     );
