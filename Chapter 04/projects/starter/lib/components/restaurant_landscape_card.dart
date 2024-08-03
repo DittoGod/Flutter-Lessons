7db34c8475e1af3a3c357d8eb3e6babe45d221ab
Slyro Dittogod <slyro@dittogod.com>
2024-08-03 20:33:57 +0100

    Added heart icon button.

--- Chapter 04/projects/starter/lib/components/restaurant_landscape_card.dart
+++ Chapter 04/projects/starter/lib/components/restaurant_landscape_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/restaurant.dart';
 
-class RestaurantLandscapeCard extends StatelessWidget {
+class RestaurantLandscapeCard extends StatefulWidget {
   final Restaurant restaurant;
 
   const RestaurantLandscapeCard({
@@ -10,7 +10,14 @@ class RestaurantLandscapeCard extends StatelessWidget {
     required this.restaurant,
   });
 
-  // TODO: Add _isFavorited property
+  @override
+  State<RestaurantLandscapeCard> createState() =>
+      _RestaurantLandscapeCardState();
+}
+
+class _RestaurantLandscapeCardState extends State<RestaurantLandscapeCard> {
+  bool _isFavourited = false;
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context)
@@ -26,25 +33,50 @@ class RestaurantLandscapeCard extends StatelessWidget {
             child: AspectRatio(
               aspectRatio: 2,
               // TODO: Convert to a stack
-              child: Image.asset(
-                restaurant.imageUrl,
-                fit: BoxFit.cover,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  // 2
+                  Image.asset(
+                    widget.restaurant.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                  // 3
+                  Positioned(
+                    top: 4.0,
+                    right: 4.0,
+                    child: IconButton(
+                      // 4
+                      icon: Icon(
+                        _isFavourited ? Icons.favorite : Icons.favorite_border,
+                      ),
+                      iconSize: 30.0,
+                      color: Colors.red[400],
+                      // 5
+                      onPressed: () {
+                        setState(() {
+                          _isFavourited = !_isFavourited;
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
           ListTile(
             title: Text(
-              restaurant.name,
+              widget.restaurant.name,
               style: textTheme.titleSmall,
             ),
             subtitle: Text(
-              restaurant.attributes,
+              widget.restaurant.attributes,
               maxLines: 1,
               style: textTheme.bodySmall,
             ),
             onTap: () {
               // ignore: avoid_print
-              print('Tap on ${restaurant.name}');
+              print('Tap on ${widget.restaurant.name}');
             },
           ),
         ],
