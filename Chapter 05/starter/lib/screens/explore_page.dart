461bacacf7a0a2f2bf1854455bc95b7179c08965
Slyro Dittogod <slyro@dittogod.com>
2024-08-12 16:33:32 +0100

    Added futurebuilder.

--- Chapter 05/starter/lib/screens/explore_page.dart
+++ Chapter 05/starter/lib/screens/explore_page.dart
@@ -9,13 +9,37 @@ class ExplorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add ListView Future Builder
-    // Display a placeholder text.
-    return const Center(
-      child: Text(
-        'Explore Page Setup',
-        style: TextStyle(fontSize: 32.0),
-      ),
+    // FutureBuilder is a widget that works with asynchronous operations,
+    // allowing you to build UI based on the latest snapshots of a Future.
+    return FutureBuilder(
+      // FutureBuilder takes a future. Using getExploreData() to fetch data,
+      // which returns an instance of ExploreData.
+      future: mockService.getExploreData(),
+      // builder() is a function that decides what the UI should look like based
+      // on the current state of Future. Which is provided by the snapshot.
+      builder: (context, AsyncSnapshot<ExploreData> snapshot){
+        // If snapshot.connectionState is done, it means the data is available
+        // to consume.
+        if (snapshot.connectionState == ConnectionState.done){
+          // Extract the data from snapshot.data, providing default values if
+          // the data is null.
+          final restaurants = snapshot.data?.restaurants ?? [];
+          final categories = snapshot.data?.categories ?? [];
+          final posts = snapshot.data?.friendPosts ?? [];
+
+          //TODO: Replace this with Restaurant Section.
+          return const Center(
+            child: SizedBox(
+              child: Text('Show RestaurantSection'),
+            ),
+          );
+        } else {
+          // if the data is not ready to consume, show loading spinner.
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
