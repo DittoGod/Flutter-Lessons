988359c117df1394dbbf492845c740d599cec255
Slyro Dittogod <slyro@dittogod.com>
2024-08-13 11:52:26 +0100

    Added ListView to contain the three sections.

--- Chapter 05/starter/lib/screens/explore_page.dart
+++ Chapter 05/starter/lib/screens/explore_page.dart
@@ -18,18 +18,37 @@ class ExplorePage extends StatelessWidget {
       future: mockService.getExploreData(),
       // builder() is a function that decides what the UI should look like based
       // on the current state of Future. Which is provided by the snapshot.
-      builder: (context, AsyncSnapshot<ExploreData> snapshot){
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         // If snapshot.connectionState is done, it means the data is available
         // to consume.
-        if (snapshot.connectionState == ConnectionState.done){
+        if (snapshot.connectionState == ConnectionState.done) {
           // Extract the data from snapshot.data, providing default values if
           // the data is null.
           final restaurants = snapshot.data?.restaurants ?? [];
           final categories = snapshot.data?.categories ?? [];
           final posts = snapshot.data?.friendPosts ?? [];
 
-          //TODO: Wrap in a ListView
-          return RestaurantSection(restaurants: restaurants);
+          // Initialize a scrollable list of widgets.
+          return ListView(
+            // shrinkWrap sizes the ListView based on its children's height.
+            shrinkWrap: true,
+            // The list scrolls vertically.
+            scrollDirection: Axis.vertical,
+            // The list contains three child list view widgets.
+            children: [
+              RestaurantSection(restaurants: restaurants),
+              // TODO: Add CategorySection
+              Container(
+                height: 300,
+                color: Colors.green,
+              ),
+              // TODO: Add PostSection
+              Container(
+                height: 300,
+                color: Colors.orange,
+              ),
+            ],
+          );
         } else {
           // If the data is not ready to consume, show loading spinner.
           return const Center(
