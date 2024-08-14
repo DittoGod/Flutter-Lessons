e7cbc0ce279bb230b8ede989d432080c4ee6d5c2
Slyro Dittogod <slyro@dittogod.com>
2024-08-14 10:33:57 +0100

    Started chapter 07.

--- Chapter 07/starter/lib/main.dart
+++ Chapter 07/starter/lib/main.dart
@@ -1,67 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'fooderlich_theme.dart';
-import 'models/models.dart';
-import 'navigation/app_router.dart';
+import 'constants.dart';
+import 'home.dart';
+import 'models/cart_manager.dart';
+import 'models/order_manager.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final appStateManager = AppStateManager();
-  await appStateManager.initializeApp();
-  runApp(Fooderlich(appStateManager: appStateManager));
+void main() {
+  runApp(const Yummy());
 }
 
-class Fooderlich extends StatefulWidget {
-  final AppStateManager appStateManager;
-
-  const Fooderlich({super.key, required this.appStateManager});
+class Yummy extends StatefulWidget {
+  const Yummy({super.key});
 
   @override
-  FooderlichState createState() => FooderlichState();
+  State<Yummy> createState() => _YummyState();
 }
 
-class FooderlichState extends State<Fooderlich> {
-  late final _groceryManager = GroceryManager();
-  late final _profileManager = ProfileManager();
-  late final _appRouter = AppRouter(
-    widget.appStateManager,
-    _profileManager,
-    _groceryManager,
-  );
+class _YummyState extends State<Yummy> {
+  ThemeMode themeMode = ThemeMode.light;
+  ColorSelection colorSelected = ColorSelection.pink;
+
+  /// Manage user's shopping cart for the items they order.
+  final CartManager _cartManager = CartManager();
+
+  /// Manage user's orders submitted
+  final OrderManager _orderManager = OrderManager();
+
+  void changeThemeMode(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode
+          ? ThemeMode.light //
+          : ThemeMode.dark;
+    });
+  }
+
+  void changeColor(int value) {
+    setState(() {
+      colorSelected = ColorSelection.values[value];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => _groceryManager,
-        ),
-        ChangeNotifierProvider(
-          create: (context) => _profileManager,
-        ),
-        ChangeNotifierProvider(
-          create: (context) => widget.appStateManager,
-        ),
-      ],
-      child: Consumer<ProfileManager>(
-        builder: (context, profileManager, child) {
-          ThemeData theme;
-          if (profileManager.darkMode) {
-            theme = FooderlichTheme.dark();
-          } else {
-            theme = FooderlichTheme.light();
-          }
+    const appTitle = 'Yummy';
 
-          final router = _appRouter.router;
-          return MaterialApp.router(
-            theme: theme,
-            title: 'Fooderlich',
-            routerDelegate: router.routerDelegate,
-            routeInformationParser: router.routeInformationParser,
-            routeInformationProvider: router.routeInformationProvider,
-          );
-        },
+    return MaterialApp(
+      title: appTitle,
+      debugShowCheckedModeBanner: false, // Uncomment to remove Debug banner
+      themeMode: themeMode,
+      theme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: Home(
+        appTitle: appTitle,
+        cartManager: _cartManager,
+        ordersManager: _orderManager,
+        changeTheme: changeThemeMode,
+        changeColor: changeColor,
+        colorSelected: colorSelected,
       ),
     );
   }
