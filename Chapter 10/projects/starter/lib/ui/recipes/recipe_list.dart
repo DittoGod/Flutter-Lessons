8c06fd8d232d3cbb571f1818da6e1530a70d63cc
Slyro Dittogod <slyro@dittogod.com>
2024-08-20 12:04:38 +0100

    Started Chapter 10.

--- Chapter 10/projects/starter/lib/ui/recipes/recipe_list.dart
+++ Chapter 10/projects/starter/lib/ui/recipes/recipe_list.dart
@@ -1,29 +1,37 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../colors.dart';
-import '../widgets/custom_dropdown.dart';
-
-import 'dart:convert';
-import '../../network/recipe_model.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../network/service_interface.dart';
+import '../widgets/common.dart';
+import '../../data/models/models.dart';
+import '../../network/model_response.dart';
+import '../../network/query_result.dart';
+import '../bookmarks/bookmarks.dart';
 import '../recipe_card.dart';
-import 'recipe_details.dart';
+import '../recipes/recipe_details.dart';
+import '../theme/colors.dart';
+import '../widgets/custom_dropdown.dart';
+// TODO: Add imports
 
-class RecipeList extends StatefulWidget {
+
+enum ListType { all, bookmarks }
+
+class RecipeList extends ConsumerStatefulWidget {
   const RecipeList({Key? key}) : super(key: key);
 
   @override
-  State createState() => _RecipeListState();
+  ConsumerState createState() => _RecipeListState();
 }
 
-class _RecipeListState extends State<RecipeList> {
-  static const String prefSearchKey = 'previousSearches';
+class _RecipeListState extends ConsumerState<RecipeList> {
+  // TODO Add Search Index Key
 
   late TextEditingController searchTextController;
   final ScrollController _scrollController = ScrollController();
-  List currentSearchList = [];
+  List<Recipe> currentSearchList = [];
   int currentCount = 0;
   int currentStartPosition = 0;
   int currentEndPosition = 20;
@@ -32,80 +40,124 @@ class _RecipeListState extends State<RecipeList> {
   bool loading = false;
   bool inErrorState = false;
   List<String> previousSearches = <String>[];
-  APIRecipeQuery? _currentRecipes1;
+  ListType currentType = ListType.all;
+  Future<RecipeResponse>? currentResponse;
+  bool newDataRequired = true;
 
   @override
   void initState() {
     super.initState();
-    loadRecipes();
     getPreviousSearches();
+
     searchTextController = TextEditingController(text: '');
     _scrollController.addListener(() {
-      final triggerFetchMoreSize =
-          0.7 * _scrollController.position.maxScrollExtent;
+      if (currentType == ListType.all) {
+        final triggerFetchMoreSize =
+            0.7 * _scrollController.position.maxScrollExtent;
 
-      if (_scrollController.position.pixels > triggerFetchMoreSize) {
-        if (hasMore &&
-            currentEndPosition < currentCount &&
-            !loading &&
-            !inErrorState) {
-          setState(() {
-            loading = true;
-            currentStartPosition = currentEndPosition;
-            currentEndPosition =
-                min(currentStartPosition + pageCount, currentCount);
-          });
+        if (_scrollController.position.pixels > triggerFetchMoreSize) {
+          if (hasMore &&
+              currentEndPosition < currentCount &&
+              !loading &&
+              !inErrorState) {
+            setState(
+                  () {
+                loading = true;
+                newDataRequired = true;
+                currentStartPosition = currentEndPosition;
+                currentEndPosition =
+                    min(currentStartPosition + pageCount, currentCount);
+              },
+            );
+          }
         }
       }
-    });
-  }
-
-  Future loadRecipes() async {
-    // Loads recipes1.json from the assets directory. rootBundle is the
-    // top-level property that holds references to all the items in the asset
-    // folder. This loads the file as a string.
-    final jsonString = await rootBundle.loadString('assets/recipes1.json');
-    setState(() {
-      // Uses the built-in jsonDecode() method to convert the string to a map,
-      // then uses fromJson(), which was generated for you, to make an instance
-      // of an APIRecipeQuery.
-      _currentRecipes1 = APIRecipeQuery.fromJson(jsonDecode(jsonString));
     });
   }
 
   @override
   void dispose() {
     searchTextController.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
   void savePreviousSearches() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setStringList(prefSearchKey, previousSearches);
+    // TODO Save Current Index
   }
 
   void getPreviousSearches() async {
-    final prefs = await SharedPreferences.getInstance();
-    if (prefs.containsKey(prefSearchKey)) {
-      final searches = prefs.getStringList(prefSearchKey);
-      if (searches != null) {
-        previousSearches = searches;
-      } else {
-        previousSearches = <String>[];
-      }
-    }
+    // TODO Get Current Index
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            _buildSearchCard(),
-            _buildRecipeLoader(context),
+    return switch (currentType) {
+      ListType.all => buildRecipeList(),
+      ListType.bookmarks => buildBookmarkList()
+    };
+  }
+
+  Widget buildRecipeList() {
+    return buildScrollList([
+      _buildHeader(),
+      _buildTypePicker(),
+      _buildSearchCard(),
+    ], _buildRecipeLoader(context));
+  }
+
+  Widget buildScrollList(List<Widget> topList, Widget bottomWidget) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ...topList,
+        ScrollConfiguration(
+          behavior: const ScrollBehavior().copyWith(
+            dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch},
+            physics: const ClampingScrollPhysics(),
+          ),
+          child: Expanded(
+            child: CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                SliverPadding(
+                  padding: allPadding8,
+                  sliver: bottomWidget,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildBookmarkList() {
+    return buildScrollList([
+      _buildHeader(),
+      _buildTypePicker(),
+    ], const Bookmarks());
+  }
+
+  Widget _buildHeader() {
+    return SizedBox(
+      height: 160.0,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: lightGreen,
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                'assets/images/background2.png',
+                fit: BoxFit.cover,
+              ),
+            ),
           ],
         ),
       ),
@@ -131,16 +183,16 @@ class _RecipeListState extends State<RecipeList> {
                 }
               },
             ),
-            const SizedBox(
-              width: 6.0,
-            ),
+            sizedW8,
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
                       child: TextField(
                     decoration: const InputDecoration(
-                        border: InputBorder.none, hintText: 'Search'),
+                      border: InputBorder.none,
+                      hintText: 'Search',
+                    ),
                     autofocus: false,
                     textInputAction: TextInputAction.done,
                     onSubmitted: (value) {
@@ -148,6 +200,14 @@ class _RecipeListState extends State<RecipeList> {
                     },
                     controller: searchTextController,
                   )),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      setState(() {
+                        searchTextController.text = '';
+                      });
+                    },
+                  ),
                   PopupMenuButton<String>(
                     icon: const Icon(
                       Icons.arrow_drop_down,
@@ -166,6 +226,7 @@ class _RecipeListState extends State<RecipeList> {
                           callback: () {
                             setState(() {
                               previousSearches.remove(value);
+                              savePreviousSearches();
                               Navigator.pop(context);
                             });
                           },
@@ -185,10 +246,11 @@ class _RecipeListState extends State<RecipeList> {
   void startSearch(String value) {
     setState(() {
       currentSearchList.clear();
+      newDataRequired = true;
       currentCount = 0;
       currentEndPosition = pageCount;
       currentStartPosition = 0;
-      hasMore = true;
+      hasMore = false;
       value = value.trim();
       if (!previousSearches.contains(value)) {
         previousSearches.add(value);
@@ -197,44 +259,168 @@ class _RecipeListState extends State<RecipeList> {
     });
   }
 
-  Widget _buildRecipeLoader(BuildContext context) {
-    // Checks to see if the list of recipes is null.
-    if (_currentRecipes1 == null || _currentRecipes1?.hits == null) {
-      return Container();
+  Sliver _buildRecipeLoader(BuildContext context) {
+    if (searchTextController.text.length < 3) {
+      return emptySliverWidget;
     }
-    // Show a loading indicator while waiting for the recipes
-    return Flexible(
-      child: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          return Center(
-            // If not, calls _buildRecipeCard() using the first item in the
-            // list.
-            child: _buildRecipeCard(context, _currentRecipes1!.hits, 0),
-          );
-        },
-      ),
+    return FutureBuilder<RecipeResponse>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return SliverFillRemaining(
+              child: Center(
+                child: Text(
+                  snapshot.error.toString(),
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 1.3,
+                ),
+              ),
+            );
+          }
+
+          loading = false;
+          final result = snapshot.data;
+          // Hit an error
+          if (result is Error) {
+            const errorMessage = 'Problems getting data';
+            return const SliverFillRemaining(
+              child: Center(
+                child: Text(
+                  errorMessage,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            );
+          }
+          final query = (result as Success).value as QueryResult;
+          inErrorState = false;
+          currentCount = query.totalResults;
+          hasMore = query.totalResults > (query.offset + query.number);
+          currentSearchList.addAll(query.recipes);
+          currentEndPosition =
+              min(query.totalResults, currentEndPosition + query.number);
+          if (currentCount == 0) {
+            return const SliverFillRemaining(
+              child: Center(
+                child: Text(
+                  'No Results',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            );
+          } else {
+            return _buildRecipeList(context, currentSearchList);
+          }
+        } else {
+          if (currentCount == 0) {
+            // Show a loading indicator while waiting for the movies
+            return const SliverFillRemaining(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            return _buildRecipeList(context, currentSearchList);
+          }
+        }
+      },
+    );
+  }
+
+  Future<RecipeResponse> fetchData() async {
+    if (!newDataRequired && currentResponse != null) {
+      return currentResponse!;
+    }
+    newDataRequired = false;
+    // TODO: Load Recipes
+/*
+    final recipeService = ref.watch(serviceProvider);
+    currentResponse = recipeService.queryRecipes(
+        searchTextController.text.trim(), currentStartPosition, pageCount);
+    return currentResponse!;
+*/
+    const apiQueryResults = QueryResult(
+        offset: 0,
+        number: 0,
+        totalResults: 0,
+        recipes: <Recipe>[]);
+    return Success(apiQueryResults);
+
+  }
+
+  Widget _buildRecipeList(
+      BuildContext recipeListContext, List<Recipe> recipes) {
+    return SliverLayoutBuilder(
+      builder: (BuildContext context, SliverConstraints constraints) {
+        final numColumns = max(1, constraints.crossAxisExtent ~/ 264);
+        return SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            childCount: recipes.length,
+            (BuildContext context, int index) {
+              return _buildRecipeCard(
+                recipeListContext,
+                recipes,
+                index,
+              );
+            },
+          ),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: numColumns, mainAxisExtent: 264),
+        );
+      },
     );
   }
 
   Widget _buildRecipeCard(
     BuildContext topLevelContext,
-    List<APIHits> hits,
+    List<Recipe> recipes,
     int index,
   ) {
-    // Finds the recipe at the given index.
-    final recipe = hits[index].recipe;
+    final recipe = recipes[index];
     return GestureDetector(
       onTap: () {
         Navigator.push(topLevelContext, MaterialPageRoute(
           builder: (context) {
-            return const RecipeDetails();
+            return RecipeDetails(recipe: recipe);
           },
         ));
       },
-      // Calls recipeStringCard(), which shows a nice card below the search
-      // field.
-      child: recipeStringCard(recipe.image, recipe.label),
+      child: recipeCard(recipe),
+    );
+  }
+
+  Widget _buildTypePicker() {
+    return IntrinsicWidth(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SegmentedButton<ListType>(
+              segments: const [
+                ButtonSegment<ListType>(
+                    value: ListType.all, label: Text('All'), enabled: true),
+                ButtonSegment<ListType>(
+                    value: ListType.bookmarks,
+                    label: Text('Bookmarks'),
+                    enabled: true),
+              ],
+              selected: {currentType},
+              onSelectionChanged: (Set<ListType> newSelection) {
+                setState(() {
+                  // By default there is only a single segment that can be
+                  // selected at one time, so its value is always the first
+                  // item in the selected set.
+                  currentType = newSelection.first;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
