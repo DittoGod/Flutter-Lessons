1225ed5559b245561801ea5717b69f78757a7d9f
Slyro Dittogod <slyro@dittogod.com>
2024-08-24 14:58:02 +0100

    Updated Envied.

--- Chapter 12/projects/starter/lib/network/env.dart
+++ Chapter 12/projects/starter/lib/network/env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: 'lib/network/.env')
+@Envied(path: '.env', useConstantCase: true)
 abstract class Env {
   @EnviedField(varName: 'apiKey')
   static const String apiKey = _Env.apiKey;
